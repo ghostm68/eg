7438bf7037be0c6ec0b11d49d4bf7989b1cfcc5e
inky <42044862+ghostm68@users.noreply.github.com>
2025-10-28 20:38:08 -0400

    Update dreamweaver.js

--- dreamweaver.js
+++ dreamweaver.js
@@ -1,33 +1,33 @@
 import React, { useState, useCallback, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
-// --- START: Icon Components ---
-const SparklesIcon = (props) => (
+// --- START: Icon Components (Corrected) ---
+const SparklesIcon = ({ ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
     <path fillRule="evenodd" d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-7.182o-.001-.001a6.72 6.72 0 01-2.22 3.182.75.75 0 01-1.07-.478A5.25 5.25 0 016 13.5v-3.75a.75.75 0 01.75-.75h3.355a2.25 2.25 0 01.188 0l.001.001h.001L9.315 7.584zM12 15.375a3.75 3.75 0 013.75-3.75H18a3.75 3.75 0 013.75 3.75v3.75a3.75 3.75 0 01-3.75 3.75H15.75a3.75 3.75 0 01-3.75-3.75v-3.75z" clipRule="evenodd" />
   </svg>
 );
 
-const DocumentTextIcon = (props) => (
+const DocumentTextIcon = ({ ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
     <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a.375.375 0 01-.375-.375V6.75A3.75 3.75 0 0010.5 3h-4.875C5.663 3 5.641 3 5.625 3zM12 9V4.875c0-.621.504-1.125 1.125-1.125H13.5v3.75c0 .621.504 1.125 1.125 1.125h3.75v9.375a.375.375 0 01-.375.375H5.625a.375.375 0 01-.375-.375V3.375c0-.207.168-.375.375-.375h4.875c.621 0 1.125.504 1.125 1.125V9z" clipRule="evenodd" />
     <path d="M10.5 13.5a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm0 3a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75z" />
   </svg>
 );
 
-const PaperAirplaneIcon = (props) => (
+const PaperAirplaneIcon = ({ ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
     <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
   </svg>
 );
 
-const ExclamationTriangleIcon = (props) => (
+const ExclamationTriangleIcon = ({ ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
     <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.598 4.5H4.644c-2.308 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
   </svg>
 );
 
-const InformationCircleIcon = (props) => (
+const InformationCircleIcon = ({ ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.042.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
   </svg>
